b3fc1591d0c5768a8ec4e530d568d95c5530d442
David Ortiz <113551897+dortiz10@users.noreply.github.com>
2023-02-15 20:28:00 +0000

    with open

--- main.py
+++ main.py
@@ -1,10 +1,55 @@
 # remember to fork this and push to github
 
-with open('test.txt', 'r') as f:
+# f = open('test.txt', 'r')
+# print(f.readlines())
+# f.close()
 
-f = open('test.txt', 'r')
+# with open('test.txt', 'r') as f:
+#   f_contents = f.readlines()
+#   print(f_contents, end='')
 
-print(f.mode)
+# employee_file = open("employees.txt", "r")
 
-f.close()
+# print(employee_file.readline()[1])
+# print(employee_file.readline())
+# print(employee_file.readline())
 
+# employee_file = open("employees.txt", "r")
+# for employee in employee_file.readlines():
+#   print(employee)
+# employee_file.close()
+
+# employee_file.close()
+
+# employee_file = open("employees.txt", "a")
+
+# employee_file.write("Toby - Human Resources")
+
+# employee_file.write("/nKelly - Customer Service")
+
+
+# employee_file.close()
+
+# employee_file = open("employees.txt", "w")
+
+# employee_file.write("/nKelly - Customer Service")
+
+# employee_file.close()
+
+# employee_file = open("omindex.html", "w")
+
+# employee_file.write("<p>This is HTML</p>")
+
+# employee_file.close()
+
+with open('readme.txt','w') as f:
+  f.write('readme')
+
+#This overwrites the original content
+with open('readme.txt','w') as f:
+  f.write("I don't care")
+
+#This appends to the original content
+with open('readme.txt','a') as f:
+  f.write("I don't care I don't care I don't care")
+  
